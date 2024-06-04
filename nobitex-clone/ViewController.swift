03a6190766ca8654383ceea92a505f1e61fb9f20
Majid Jamali <majidjamali007@gmail.com>
2024-06-04 10:58:25 +0330

    Switching Project to UIKit / Programmatically
    More Control over Views and less hassle in code indentation is a plus for UIKit over SwiftUI.

---
+++ nobitex-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nobitex-clone
+//
+//  Created by Majid Jamali on 2024/6/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
